ad93487bb251802d1379dbaea9561d3c8fab8c9a
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 06:55:35 +0700

    Kode ini untuk mempermudah pengelolaan rute navigasi, terdapat hal. home, detail, dan update

--- app/src/main/java/com/example/prak8/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/prak8/ui/navigation/AlamatNavigasi.kt
@@ -3,5 +3,20 @@ package com.example.prak8.ui.navigation
 interface AlamatNavigasi {          // untuk menyimpan riuting halaman
     val route: String
 }
+object DestinasiHome : AlamatNavigasi{
+    override val route= "home"
+}
 
+object DestinasiDetail : AlamatNavigasi{
+    override val route= "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+
+object DestinasiUpdate : AlamatNavigasi{
+    override val route= "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
 
