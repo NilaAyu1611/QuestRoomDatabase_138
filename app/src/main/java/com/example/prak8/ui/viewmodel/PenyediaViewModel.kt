1859d574f72e9e9fdc1fb30de9a4f581ee99beeb
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 07:27:53 +0700

    meyimpan perubahan scr otomatis dan mengambil nim dari navigasu yang dikirim melalui savedstatehandle. serta memperbarui state berdasarkan input user.

--- app/src/main/java/com/example/prak8/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak8/ui/viewmodel/PenyediaViewModel.kt
@@ -19,4 +19,25 @@ class UpdateMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ): ViewModel() {
+    var updateUIState by mutableStateOf(MhsUIState())       // menyimpan state terkini (update form)
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
 }
