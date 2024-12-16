b0edcd9c65bf672ccddc246d7b594e66589f6a48
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 07:35:25 +0700

    fungsi ini mendefinisikan sbg kelas App dan registrasikan dlm androidmanisfest.

---
+++ app/src/main/java/com/example/prak8/ui/view/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,13 @@
+package com.example.prak8.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.prak8.KrsApp
+
+
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
