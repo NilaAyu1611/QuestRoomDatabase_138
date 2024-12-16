1e973456133aa3898594e2ef1fd78b0ea53ac25c
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 07:20:42 +0700

    state untuk menampilkan data detail mhs di UI

--- app/src/main/java/com/example/prak8/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/prak8/ui/viewmodel/DetailMhsViewModel.kt
@@ -65,3 +65,18 @@ class DetailMhsViewModel (
 }
 
 
+class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+/* Data class untuk menampung data yang akan ditampiljan di UI*/
+
